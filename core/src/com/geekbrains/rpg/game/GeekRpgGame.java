f2e8d52c1d0749d1f0fdd2df25a774eb7ea9ce83
Valentin Izotov <devices11@gmail.com>
2020-03-01 10:54:51 +0300

    Lesson2

--- core/src/com/geekbrains/rpg/game/GeekRpgGame.java
+++ core/src/com/geekbrains/rpg/game/GeekRpgGame.java
@@ -4,28 +4,32 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class GeekRpgGame extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture textureGrass;
-    private Texture texturePointer;
-    private Vector2 pointerPosition;
-    private float rt;
+    private BitmapFont font32;
+    private TextureAtlas atlas;
+    private TextureRegion textureGrass;
     private Hero hero;
 
     // Домашнее задание:
-    // - Разобраться с кодом
-    // - Персонаж должен двигаться к указателю
+    // 0. Разобраться с кодом
+    // 1. Добавить на экран яблоко, и попробовать отследить попадание
+    // стрелы в яблоко, при попадании яблоко должно появиться в новом месте
+    // 2. ** Попробуйте заставить героя выпускать по несколько стрел
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        hero = new Hero();
-        textureGrass = new Texture("grass.png");
-        texturePointer = new Texture("pointer.png");
-        pointerPosition = new Vector2(0, 0);
+        this.batch = new SpriteBatch();
+        this.atlas = new TextureAtlas("game.pack");
+        this.hero = new Hero(atlas);
+        this.textureGrass = atlas.findRegion("grass");
+        this.font32 = new BitmapFont(Gdx.files.internal("font32.fnt"));
     }
 
     @Override
@@ -40,17 +44,13 @@ public class GeekRpgGame extends ApplicationAdapter {
                 batch.draw(textureGrass, i * 80, j * 80);
             }
         }
-        batch.draw(texturePointer, pointerPosition.x - 32, pointerPosition.y - 32, 32, 32, 64, 64, 1, 1, rt, 0, 0, 64, 64, false, false);
         hero.render(batch);
+        hero.renderGUI(batch, font32);
         batch.end();
     }
 
     public void update(float dt) {
-        rt -= dt * 90.0f;
         hero.update(dt);
-        if (Gdx.input.justTouched()) {
-            pointerPosition.set(Gdx.input.getX(), 720.0f - Gdx.input.getY());
-        }
     }
 
     @Override
--- core/src/com/geekbrains/rpg/game/Hero.java
+++ core/src/com/geekbrains/rpg/game/Hero.java
@@ -3,36 +3,68 @@ package com.geekbrains.rpg.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class Hero {
-    private Texture texture;
+    private Projectile projectile;
+    private TextureRegion texture;
+    private TextureRegion texturePointer;
+    private TextureRegion textureHp;
     private Vector2 position;
+    private Vector2 dst;
+    private Vector2 tmp;
+    private float lifetime;
     private float speed;
+    private int hp;
+    private int hpMax;
+    private StringBuilder strBuilder;
 
-    public Hero() {
-        this.texture = new Texture("hero.png");
+    public Hero(TextureAtlas atlas) {
+        this.texture = atlas.findRegion("knight");
+        this.texturePointer = atlas.findRegion("pointer");
+        this.textureHp = atlas.findRegion("hp");
         this.position = new Vector2(100, 100);
-        this.speed = 100.0f;
+        this.projectile = new Projectile(atlas);
+        this.dst = new Vector2(position);
+        this.tmp = new Vector2(0, 0);
+        this.speed = 300.0f;
+        this.hpMax = 10;
+        this.hp = 10;
+        this.strBuilder = new StringBuilder();
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x - 32, position.y - 32,32,32,64,64,1,1,0,0,0,64,64,false,false);
+        batch.draw(texturePointer, dst.x - 30, dst.y - 30, 30, 30, 60, 60, 0.5f, 0.5f, lifetime * 90.0f);
+        batch.draw(texture, position.x - 30, position.y - 30, 30, 30, 60, 60, 1, 1, 0);
+        batch.draw(textureHp, position.x - 30, position.y + 30, 60 * ((float) hp / hpMax), 12);
+        projectile.render(batch);
+    }
+
+    public void renderGUI(SpriteBatch batch, BitmapFont font) {
+        strBuilder.setLength(0);
+        strBuilder.append("Class: ").append("Knight").append("\n");
+        strBuilder.append("HP: ").append(hp).append(" / ").append(hpMax).append("\n");
+        font.draw(batch, strBuilder, 10, 710);
     }
 
     public void update(float dt) {
-        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-            position.x -= speed * dt;
+        projectile.update(dt);
+        lifetime += dt;
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            dst.set(Gdx.input.getX(), 720.0f - Gdx.input.getY());
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-            position.x += speed * dt;
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
+            projectile.setup(position.x, position.y, Gdx.input.getX(), 720.0f - Gdx.input.getY());
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)) {
-            position.y -= speed * dt;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-            position.y += speed * dt;
+        tmp.set(dst).sub(position).nor().scl(speed); // вектор скорости
+        if (position.dst(dst) > speed * dt) {
+            position.mulAdd(tmp, dt);
+        } else {
+            position.set(dst);
         }
     }
 }
